083df690dfefe6022dbd825cb24d2e6028622b45
ShahadathAlam <shahadathalam@ymail.com>
2024-10-12 12:08:17 +0600

    Error Handling in Form Actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,12 +33,18 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const formErrors = useActionData();
+
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      {/* <h2>Ready to order? Let's go!</h2> */}
+      <h2>Ready to order? Let&apos;s go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
@@ -52,6 +58,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +81,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order now "}
+          </button>
         </div>
       </Form>
     </div>
@@ -90,6 +99,15 @@ export async function action({ request }) {
     priority: data.priority === "on",
     cart: JSON.parse(data.cart),
   };
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is Okay then create new order and redirect
+
   const newOrder = await createOrder(order);
   // console.log(newOrder);
   return redirect(`/order/${newOrder.id}`);
