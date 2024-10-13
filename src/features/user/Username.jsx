f571d29dba1690a2326d1ebdd17b6ebfd70dbde2
ShahadathAlam <shahadathalam@ymail.com>
2024-10-13 12:18:58 +0600

    Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Shahadath</div>;
+  return <div className="hidden text-sm font-semibold md:block">Shahadath</div>;
 }
 
 export default Username;
