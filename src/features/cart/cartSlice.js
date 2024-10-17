160d8251486a2edf40de037412382d7a3895a88e
ShahadathAlam <shahadathalam@ymail.com>
2024-10-17 21:33:09 +0600

    Updating Cart Quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -39,6 +39,13 @@ const cartSlice = createSlice({
 
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+
+      // if (item.quantity === 0)
+      //   state.cart = state.cart.filter(
+      //     (item) => item.pizzaId !== action.payload,
+      //   );
     },
     clearCart(state) {
       state.cart = [];
